32e37d6ac40870873f07c4afcf6fc05fc490ad2a
Gabrielly Candido <gabriellycasilva@gmail.com>
2025-08-05 20:57:23 -0300

    Add files via upload

---
+++ aula1_java_Primeiro_app/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Criando uma variável
+        var nome = "Gabrielly";
+
+        //Fazendo o output do dado - print
+        System.out.println("Olá " + nome);
+        //Atalho para o print: souf - após o enter ele já traz o print
+
+        //Sem o ln ele não pula a linha
+        var sobrenome = "Candido";
+        System.out.print(nome);
+        System.out.print(sobrenome);
+
+        //Console é um meio de comunicação entre o programador e o sistema
+
+        //Código amarelo= são as anotações
+        //Podemos colocar um dado dentro de um dado com as anotações, facilidando na hora de colocar o código
+        //https://start.spring.io/ - usaremos muito para criar projetos
+
+
+
+    }
+}
